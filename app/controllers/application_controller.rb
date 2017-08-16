5c49ab1338089799712a620d07213871ad54eb6f
forever_ram <ramsharma.forever@gmail.com>
2017-08-16 17:39:10 +0530

    merchant signup process using tokem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :authenticate_merchant_token, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     resource.class.name == "AdminUser" ? admins_root_path : root_path
+  end
+
+  def authenticate_merchant_token
+    if params[:controller] == 'devise/registrations' && ["new", "create"].include?(params[:action])
+      flash[:error] = if params[:action] == "new" && params[:token].blank?
+        "You can't signup without token"
+      elsif params[:action] == "create"
+        merchant = Merchant.where(token: params[:user][:token]).first
+        "Invalid token" if merchant.blank?
+      end
+      redirect_to root_path if flash[:error]
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:locale, :token])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :token
+
+  after_create :assign_user_to_merchant
 
   def active_for_authentication?
     #remember to call the super
     #then put our own check to determine "active" state using
     #our own "is_active" column
     super && self.is_active?
+  end
+
+  def assign_user_to_merchant
+    merchant = Merchant.where(token: self.token).first
+    merchant.update_attributes(user_id: self.id)
   end
 
 end
