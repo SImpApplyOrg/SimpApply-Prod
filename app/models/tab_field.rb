e01491d1256bf41f7fa9db94166ecafab2278741
forever_ram <ramsharma.forever@gmail.com>
2017-08-30 22:38:35 +0530

    SIMP-93 added validation

--- app/models/tab_field.rb
+++ app/models/tab_field.rb
@@ -1,5 +1,6 @@
 class TabField < ApplicationRecord
   belongs_to :screen_tab
   belongs_to :job_application_question
-  validates :job_application_question_id, presence: true
+
+  validates_presence_of :job_application_question_id
 end
--- app/models/view_screen.rb
+++ app/models/view_screen.rb
@@ -3,15 +3,32 @@ class ViewScreen < ApplicationRecord
   enum screen_for: { job_application: 'Job Application' }
 
   has_many :screen_tabs, dependent: :destroy
-  accepts_nested_attributes_for :screen_tabs, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :screen_tabs, allow_destroy: true
   has_many :tab_fields, through: :screentabs
 
   validates_presence_of :screen_for
+  validates_uniqueness_of :screen_for, if: 'screen_for.present?'
+  validate :check_screen_tabs_position
 
   after_initialize :assign_screen_for
 
   private
     def assign_screen_for
       self.screen_for = 'job_application' if self.new_record?
+    end
+
+    def check_screen_tabs_position
+      positions = screen_tabs.pluck(:position)
+      screen_tabs.each do |screen_tab|
+        if screen_tab.new_record?
+          if positions.include?(screen_tab.position)
+            errors.add(:'screen_tabs.position', 'has already been taken')
+          else
+            positions << screen_tab.position
+          end
+
+          errors.add(:'screen_tabs.job_application_question', 'select at-least one') if screen_tab.tab_fields.blank?
+        end
+      end
     end
 end
