5d127c747a657430275c5832659fd3f327ecd912
forever_ram <ramsharma.forever@gmail.com>
2017-09-12 16:28:56 +0530

    fix issue on user signup 'Email blank'

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   attr_accessor :token
 
   after_initialize :assign_default_values
-  after_create :assign_user_to_merchant
   before_create :assign_default_values
+  after_create :assign_user_to_merchant
 
   has_one :merchant
   has_many :applicants, through: :merchant
@@ -32,8 +32,8 @@ class User < ApplicationRecord
         merchant = Merchant.where(token: self.token).first
 
         if merchant
-          self.mobile_no = merchant.mobile_no
-          self.email = merchant.email
+          self.mobile_no = merchant.mobile_no if merchant.mobile_no.present?
+          self.email = merchant.email if merchant.email.present?
         end
       end
     end
