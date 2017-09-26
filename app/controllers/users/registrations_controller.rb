acd4718bc1c4d48a94c1df9f9693f65e216fc0de
forever_ram <ramsharma.forever@gmail.com>
2017-09-26 22:47:40 +0530

    SIMP-126 add merchant first and last name on signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,7 +16,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_up_params
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:locale, :token, :mobile_no])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:locale, :token, :mobile_no, :first_name, :last_name])
     end
 
     def authenticate_merchant_token
