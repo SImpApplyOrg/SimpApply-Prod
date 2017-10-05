5c1b58cd84577bc57c6684acc2c063014ec50f67
forever_ram <ramsharma.forever@gmail.com>
2017-10-05 22:05:43 +0530

    SIMP-114 few fixes

--- app/models/job_application_question.rb
+++ app/models/job_application_question.rb
@@ -1,7 +1,7 @@
 class JobApplicationQuestion < ApplicationRecord
   has_many :tab_fields, dependent: :destroy
 
-  PDF_MAPPING = ['1', '2', '3', '4', '5', '6', '7', '8', '22', '23', '18A', '18B', '18C', '18D', '18E', '18F', '18G', '18L', '19A', '19B', '19C', '19D', '19E', '19F', '19G', '19L', '20A', '20B', '20C', '20D', '20E', '20F', '20G', '20L', '21A', '21B', '21C', '22', '23', '24']
+  PDF_MAPPING = ['1', '2', '3', '4', '5', '6', '7', '8', '18A', '18B', '18C', '18D', '18E', '18F', '18G', '18L', '19A', '19B', '19C', '19D', '19E', '19F', '19G', '19L', '20A', '20B', '20C', '20D', '20E', '20F', '20G', '20L', '21A', '21B', '21C', '22', '23', '24']
 
   scope :type_form_questions, -> { where("field_id IS NOT NULL") }
   scope :custom_questions, -> { where("field_id IS NULL") }
